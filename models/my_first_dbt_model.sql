19be462aea0522994d22aa3886facfc0a9fee000
itsmyname <myemail@gmail.com>
2025-05-01 22:10:25 +0530

    created 2 more table called order,payments to join with customers table for dbt_postgres_project

--- models/my_first_dbt_model.sql
+++ models/my_first_dbt_model.sql
@@ -15,22 +15,50 @@
 WITH customers_cte AS (
     SELECT
         id,
-        first_name,
-        last_name,
+        name,
         email,
-        created_at,
-        updated_at
-FROM {{ source('raw_data', 'customers') }}
+        created_at 
+    -- FROM customers
+    FROM {{ source('raw_data', 'customers') }}
+),
+
+orders_cte AS (
+    SELECT
+        id,
+        customer_id,
+        order_date,
+        status
+	--FROM orders
+    FROM {{ source('raw_data', 'orders') }}
+),
+
+payments_cte AS (
+    SELECT
+        id,
+        order_id,
+        amount,
+        payment_date,
+        method
+	--FROM payments
+    FROM {{ source('raw_data', 'payments') }}
 )
 
 SELECT
-    id,
-    first_name,
-    last_name,
-    email,
-    created_at,
-    updated_at
-FROM customers_cte
+    c.id AS customer_id,
+    c.name AS customer_name,
+    c.email,
+    c.created_at AS customer_since,
+    o.id AS order_id,
+    o.order_date,
+    o.status AS order_status,
+    p.id AS payment_id,
+    p.amount,
+    p.payment_date,
+    p.method AS payment_method
+FROM customers_cte c
+LEFT JOIN orders_cte o ON c.id = o.customer_id
+LEFT JOIN payments_cte p ON o.id = p.order_id
+ORDER BY c.id, o.id, p.id
 
 
 /*
